637eeb3e46e6c0fab87df2288bf9e6e24a3c46ad
Glori0usOCE <glori0us.oce@gmail.com>
2023-12-02 21:43:41 +1100

    Completed Day 2

--- 02/part2.py
+++ 02/part2.py
@@ -13,6 +13,34 @@ for line in gameRaw:
 for game in gameList:
     formattedGameList.append(re.split('[,;]+', game))
 
+for game in formattedGameList:
+    minRed = 0
+    minBlue = 0
+    minGreen = 0
+    power = 0
+    for pebbles in game:
+        validityCheck = ''
+
+        for char in pebbles:
+            if char.isnumeric():
+                validityCheck += char
+
+        if pebbles.find('red') != -1:
+            if int(validityCheck) > minRed:
+                minRed = int(validityCheck)
+
+        elif pebbles.find('green') != -1:
+            if int(validityCheck) > minGreen:
+                minGreen = int(validityCheck)
+
+        elif pebbles.find('blue') != -1:
+            if int(validityCheck) > minBlue:
+                minBlue = int(validityCheck)
+    power = minRed * minGreen * minBlue
+    setPower.append(power)
+
+for power in setPower:
+    finalCalibrationValue += power
 
 
 print("Final Calibration Value: " + str(finalCalibrationValue))
